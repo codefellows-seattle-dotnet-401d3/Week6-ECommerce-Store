37017bfed1c32c36c4cba138d238ef32c784fd31
philipwerner <philip.r.werner@gmail.com>
2018-04-24 11:26:21 -0700

    most of the app is built up, stopping for lunch

---
+++ ECommerce/ECommerce/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "The About Page";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "The contact page";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

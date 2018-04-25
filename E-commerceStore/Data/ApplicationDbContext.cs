5fefba192af8372f034aabb65eeb77f537a981e4
Playingtygre <kymiehsu123@gmail.com>
2018-04-25 09:37:32 -0700

    Base for Identity

---
+++ E-commerceStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using EcommerceStore.Models;
+
+namespace EcommerceStore.Data
+{
+    /* Creating the database to which to store profiles of authorized users.
+     */
+    public class ApplicationDbContext : IdentityDbContext <ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+
+
+    }
+}

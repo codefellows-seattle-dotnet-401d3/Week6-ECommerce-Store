e03dfd296203240f0a7ed14a0aa8b0f089de9d96
Playingtygre <kymiehsu123@gmail.com>
2018-04-26 13:59:06 -0700

    Last Pushing

---
+++ E-commerceStore/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace EcommerceStore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        /*This controller should only be accessed if the user is verified
+         * CRUD 
+         */
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}

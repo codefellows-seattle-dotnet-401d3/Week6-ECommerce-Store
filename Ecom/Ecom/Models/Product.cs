d4969a4f5f4ac98a630294274439bb921d64342c
Kevin Farrow <kevnaf@gmail.com>
2018-05-03 12:36:04 -0700

    products seeded and viewable

--- Ecom/Ecom/Models/Product.cs
+++ Ecom/Ecom/Models/Product.cs
@@ -7,9 +7,10 @@ namespace Ecom.Models
 {
     public class Product
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public double Cost { get; set; }
+        public decimal Cost { get; set; }
         public string Url { get; set; }
     }
 }
---
+++ Ecom/Ecom/Models/SeedProducts.cs
@@ -0,0 +1,30 @@
+﻿using Ecom.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecom.Models
+{
+    public class SeedProducts
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ProductDbContext(serviceProvider.
+                GetRequiredService<DbContextOptions<ProductDbContext>>()))
+            {
+                if (context.Products.Any()) return; // No seed needed
+
+                context.Products.AddRange(
+                    new Product { Name = "Fighter Gear", Description = "All you need", Cost = 10.99m, Url = "Test URL"},
+                    new Product { Name = "Rogue Gear", Description = "All you need", Cost = 10.99m, Url = "Test URL" },
+                    new Product { Name = "Ranger Gear", Description = "All you need", Cost = 10.99m, Url = "Test URL" },
+                    new Product { Name = "Wizard Gear", Description = "All you need", Cost = 10.99m, Url = "Test URL" }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}

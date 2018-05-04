46d391eb40ff7a98bbefc1a93c312bdd75cb3856
Playingtygre <kymiehsu123@gmail.com>
2018-05-03 21:44:14 -0700

    adding

---
+++ Emusic/Data/ProductDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Emusic.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Emusic.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products {get; set;}
+
+    }
+}

516360bc3e38065132eb192f767dcdf6e8c97271
Playingtygre <kymiehsu123@gmail.com>
2018-05-04 21:19:38 -0700

    Adding Registar

--- Emusic/Models/ApplicationUsers.cs
+++ Emusic/Models/ApplicationUsers.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Emusic.Models
 {
@@ -12,11 +13,23 @@ namespace Emusic.Models
         public string LastName { get; set; }
         public DateTime BirthDate { get; set; }
         public string FavoriteColor { get; set; }
+        public string MusicType { get; set; }
     }
 
     public static class ApplicationRoles
     {
         public const string Admin = "Admin";
         public const string Member = "Member";
+    }
+
+    public enum MusicType
+    {
+        Pop,
+        Jazz,
+        Rock,
+        Alternative,
+        Country,
+        [Display(Name = "Rythm and blues")] RB,
+
     }
 }
